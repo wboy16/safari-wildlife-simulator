6b24d3e0f48a42e3175e502e7b238988396c1f19
weedboy16 <gamefreak148d@gmail.com>
2026-07-28 19:06:06 +0200

    feat: implement Etosha wildlife spotter using random choise module and string arrays

---
+++ safari-wildlife-simulator.py
@@ -0,0 +1,10 @@
+# The Safari Wildlife Spotter
+import random
+
+wild_animals = ["Lion", "Leopard", "Rhino", "Elephant", "Buffalo", "Hippo", "Crocodile", "Kudu", "Monkey"]
+
+random_animal = random.choice(wild_animals)
+
+print("=" * 3 + " ETOSHA WILDLIFE SIMULATOR " + "=" * 3)
+print("Starting the engines... Look out the window!")
+print(f"[SPOTTED] A majestic {random_animal} has appeared near the watering hole!")
